b3cc0d04218cd60278dedafe2677f949f844b804
kurita <kurita.dev@gmail.com>
2013-09-25 18:31:02 +0900

    initialcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hairmo::Application.config.secret_token = '13859acad39b7eda91ea5f2cffa7fc0cbe0c0e67c1374a89b14ef6dfc1803831b76ee526e86182ecc4ff1897a579f6f17b96ff7a404a52ace7b1f4b79caaf310'
