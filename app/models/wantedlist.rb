cc3fb90bc9a363b0b5f29d33f33e35b868a1af18
kurita <kurita.dev@gmail.com>
2013-10-02 21:36:37 +0900

    error_message

--- app/models/wantedlist.rb
+++ app/models/wantedlist.rb
@@ -1,4 +1,11 @@
 class Wantedlist < ActiveRecord::Base
   attr_accessible :cosmeticians_id, :days, :hair_style, :memo, :name, :price, :sex
   belongs_to :cosmeticians
+
+
+  validates :name,presence:true,length:{ maximum:30 }
+  validates :hair_style,presence:true
+  validates :price,presence:true
+  validates :sex,presence:true
+  validates :days,presence:true
 end
